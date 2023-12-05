9da34e2c3e018aa87dd9f8a62d31cf564d0dabf6
Sabbir <61047782+nasirsabbir07@users.noreply.github.com>
2023-12-05 17:53:37 +0600

    take user inputs to log in form

--- frontend/ecom/src/app/components/login/login.component.ts
+++ frontend/ecom/src/app/components/login/login.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { response } from 'express';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  authService = inject(AuthService);
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  loginUser() {
+    this.authService.login(
+      this.loginForm.value.email ?? '',
+      this.loginForm.value.password ?? ''
+    );
+  }
+}
