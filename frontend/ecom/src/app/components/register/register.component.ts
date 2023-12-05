559af8627feafc32480f05bbb8f9e9ae9fe1ede7
Sabbir <61047782+nasirsabbir07@users.noreply.github.com>
2023-12-05 17:49:30 +0600

    registration functionality added

--- frontend/ecom/src/app/components/register/register.component.ts
+++ frontend/ecom/src/app/components/register/register.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { response } from 'express';
+import { error } from 'console';
+import { User } from '../../interfaces/auth';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  authService = inject(AuthService);
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  submitApplication() {
+    const postData = this.applyForm.value;
+    this.authService.registerUser(postData as User).subscribe(
+      (response) => console.log(response),
+      (error) => console.log(error)
+    );
+  }
+}
