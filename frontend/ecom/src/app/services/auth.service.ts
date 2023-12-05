5d2289f8bfdda5fec83ee549be7c6c85d0d384ba
Sabbir <61047782+nasirsabbir07@users.noreply.github.com>
2023-12-05 17:52:51 +0600

    updated for registration and login

--- frontend/ecom/src/app/services/auth.service.ts
+++ frontend/ecom/src/app/services/auth.service.ts
@@ -1,9 +1,65 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../interfaces/auth';
+import { Observable } from 'rxjs';
+import { subscribe } from 'node:diagnostics_channel';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private baseUrl = 'http://localhost:3000';
 
-  constructor() { }
+  // isLoggedIn = false;
+  constructor(private http: HttpClient) {}
+
+  registerUser(userDetails: User) {
+    return this.http.post(`${this.baseUrl}/users`, userDetails);
+  }
+
+  // getUserByEmail(email: string): Observable<User[]> {
+  //   return this.http.get<User[]>(`${this.baseUrl}/users?email=${email}`);
+  // }
+
+  // getUsers(): Observable<{ users: User[] }> {
+  //   return this.http.get<{ users: User[] }>(`${this.baseUrl}/users`);
+  // }
+
+  // login(email: string, password: string): boolean {
+  //   this.getUsers().subscribe((data) => {
+  //     const foundUser = data.users.find(
+  //       (user: { email: string; password: string }) => {
+  //         return user.email == email && user.password == password;
+  //       }
+  //     );
+
+  //     if (foundUser) {
+  //       this.isLoggedIn = true;
+  //       console.log(foundUser);
+  //       return true;
+  //     } else {
+  //       return false;
+  //     }
+  //   });
+  //   return false;
+  // }
+
+  getUsers(): Observable<{ users: User[] }> {
+    return this.http.get<{ users: User[] }>(`${this.baseUrl}/users`);
+  }
+
+  login(useremail: string, userpassword: string) {
+    this.getUsers().subscribe((data) => {
+      const foundUser = data.users.find(
+        (user: { email: string; password: string }) => {
+          return user.email === useremail && user.password === userpassword;
+        }
+      );
+      if (foundUser) {
+        console.log('found user');
+      } else {
+        console.log('user not found');
+      }
+    });
+  }
 }
