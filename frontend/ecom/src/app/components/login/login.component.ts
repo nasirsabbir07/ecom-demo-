7f89c5f47f2132ffa7ef683f24f9f2f1b02044f1
Sabbir <61047782+nasirsabbir07@users.noreply.github.com>
2023-12-06 13:01:39 +0600

    updated to reroute to home page after log in

--- frontend/ecom/src/app/components/login/login.component.ts
+++ frontend/ecom/src/app/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 import { response } from 'express';
 
 @Component({
@@ -12,16 +13,22 @@ import { response } from 'express';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  authService = inject(AuthService);
+  constructor(private authService: AuthService, private router: Router) {}
+
   loginForm = new FormGroup({
     email: new FormControl(''),
     password: new FormControl(''),
   });
 
   loginUser() {
-    this.authService.login(
-      this.loginForm.value.email ?? '',
-      this.loginForm.value.password ?? ''
-    );
+    const email = this.loginForm.value.email;
+    const password = this.loginForm.value.password;
+
+    if (email && password) {
+      this.authService.login(email, password);
+      this.router.navigate(['/home']);
+    } else {
+      console.error('invalid email or password');
+    }
   }
 }
