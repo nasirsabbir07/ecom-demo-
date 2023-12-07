044b5b2f0b5a2490045b3175003b0db41d34060f
Sabbir <61047782+nasirsabbir07@users.noreply.github.com>
2023-12-07 17:15:39 +0600

    imported necessary components

--- frontend/ecom/src/app/components/home/home.component.ts
+++ frontend/ecom/src/app/components/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from '../shared/header/header.component';
+import { FooterComponent } from '../shared/footer/footer.component';
+import { ProductsComponent } from '../shared/products/products.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, HeaderComponent, FooterComponent, ProductsComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
