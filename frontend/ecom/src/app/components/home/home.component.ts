887d956dd004c1cc98911fd3e4368bacdc5259e1
Sabbir <61047782+nasirsabbir07@users.noreply.github.com>
2023-12-11 17:36:28 +0600

    fetch and display product json data

--- frontend/ecom/src/app/components/home/home.component.ts
+++ frontend/ecom/src/app/components/home/home.component.ts
@@ -1,14 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from '../shared/header/header.component';
 import { FooterComponent } from '../shared/footer/footer.component';
 import { ProductsComponent } from '../shared/products/products.component';
-
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../interfaces/product';
+import { error } from 'console';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, HeaderComponent, FooterComponent, ProductsComponent],
-  templateUrl: './home.component.html',
+  imports: [
+    RouterModule,
+    HeaderComponent,
+    FooterComponent,
+    ProductsComponent,
+    CommonModule,
+  ],
+  templateUrl: `./home.component.html`,
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  productList: Product[] = [];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.fetchProducts();
+  }
+
+  fetchProducts(): void {
+    this.productService.getProducts().subscribe(
+      (products: Product[]) => {
+        console.log('Received products:', products);
+        if (products && products.length > 0) {
+          this.productList = products;
+          console.log('Product list:', this.productList);
+        } else {
+          console.log('No products received or empty array.');
+        }
+      },
+      (error) => {
+        console.error('Error fetching products:', error); // Log detailed error
+      },
+    );
+  }
+}
