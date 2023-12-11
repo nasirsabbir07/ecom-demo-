87179ee709d36787db017a150af5ebf94b71cdd0
Sabbir <61047782+nasirsabbir07@users.noreply.github.com>
2023-12-11 17:38:37 +0600

    take inputs of the products

--- frontend/ecom/src/app/components/shared/products/products.component.ts
+++ frontend/ecom/src/app/components/shared/products/products.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../../../interfaces/product';
 
 @Component({
   selector: 'app-products',
   standalone: true,
   imports: [],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrl: './products.component.css',
 })
 export class ProductsComponent {
-
+  @Input() product!: Product;
 }
