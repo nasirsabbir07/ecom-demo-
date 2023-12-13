32f5120dbafa6b186b2ecd0a9b7be147e4b749d9
Nasir <nasirsabbir07@gmail.com>
2023-12-13 13:08:17 +0600

    firebase setup

--- frontend/ecom/src/app/app.config.ts
+++ frontend/ecom/src/app/app.config.ts
@@ -1,14 +1,25 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { firebaseConfig } from '../firebase.config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
     provideHttpClient(withFetch()),
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    ),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideStorage(() => getStorage())),
   ],
 };
