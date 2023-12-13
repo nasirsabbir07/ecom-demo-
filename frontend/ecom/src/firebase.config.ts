4d6fad90f33764ab16cb64f705d139808269bfef
Nasir <nasirsabbir07@gmail.com>
2023-12-13 13:08:28 +0600

    firebase setup

---
+++ frontend/ecom/src/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAcH9tRivDnjBVVghom3HlmmUyk4IyOGmk',
+  authDomain: 'ecom-demo-9cef5.firebaseapp.com',
+  projectId: 'ecom-demo-9cef5',
+  storageBucket: 'ecom-demo-9cef5.appspot.com',
+  messagingSenderId: '505717193984',
+  appId: '1:505717193984:web:e7300d37e9aef5053607e5',
+};
